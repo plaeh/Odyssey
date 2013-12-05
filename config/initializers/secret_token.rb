d490655526c5fa5b416120474e09793602428b69
Alexia McKenzie <pandapprentice@gmail.com>
2013-12-04 21:32:46 -0800

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odyssey::Application.config.secret_key_base = 'eff0c0590a38f6c54bbcff3edb872cf8d93b301bd9b016d4c0c2bdaf5258c0139ddd3165826948453272e5b4a0b3834918e4bafd76837b6e50c744d09673efed'
